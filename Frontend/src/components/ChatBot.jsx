5b1e0f1106babe60466b03f2e6fa5748b29afc17
IvanYuantama <123520791+IvanYuantama@users.noreply.github.com>
2025-07-08 13:41:10 +0700

    Update ChatBot.jsx

--- Frontend/src/components/ChatBot.jsx
+++ Frontend/src/components/ChatBot.jsx
@@ -27,9 +27,25 @@ const ChatBot = () => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          model: "deepseek/deepseek-r1:free",
-          messages: [{ role: "user", content: input }],
-        }),
+    "model": "google/gemini-2.5-pro",
+    "messages": [
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "text",
+            "text": input
+          },
+          {
+            "type": "image_url",
+            "image_url": {
+              "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
+            }
+          }
+        ]
+      }
+    ]
+  }),
       });
 
       const data = await res.json();
